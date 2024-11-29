7b6aa42cda477c53f179b8a0669f05de5d0d118f
axyut <aoogleak@gmail.com>
2024-11-29 17:00:57 +0545

    feat: basic auth and home

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,31 @@ import { TaskCreate } from "./endpoints/taskCreate";
 import { TaskDelete } from "./endpoints/taskDelete";
 import { TaskFetch } from "./endpoints/taskFetch";
 import { TaskList } from "./endpoints/taskList";
+import { basicAuth } from "hono/basic-auth";
+import { env } from "hono/adapter";
 
-// Start a Hono app
-const app = new Hono();
+type Bindings = {
+  USERNAME: string;
+  PASSWORD: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use("/admin/*", async (c, next) => {
+  const auth = basicAuth({
+    username: c.env.USERNAME,
+    password: c.env.PASSWORD,
+  });
+  return auth(c, next);
+});
+
+app.get("/admin", (c) => {
+  return c.text("You are authorized!");
+});
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
-  docs_url: "/",
+  docs_url: "/api",
 });
 
 // Register OpenAPI endpoints
@@ -19,7 +37,41 @@ openapi.post("/api/tasks", TaskCreate);
 openapi.get("/api/tasks/:taskSlug", TaskFetch);
 openapi.delete("/api/tasks/:taskSlug", TaskDelete);
 
-app.get("/hello", (c) => c.text("Hello, World!"));
+app.get("/", (c) => {
+  // const val = c.get("result"); // val is a string
+  // const username = c.env.USERNAME;
+  const username = env(c).USERNAME;
+  console.log(username);
+  return c.html(`
+    <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="club, opensource" />
+    <meta name="viewport" content="width=device-width" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+    <title>BOSC-API</title>
+    <meta
+      name="description"
+      content="BOSC is a club for opensource enthusiasts from Birendra Multiple Campus. This is the BOSC's official website's API."
+    />
+
+  </head>
+  <body>
+  <h1>Welcome to BOSC API</h1>
+  <p>
+  This is the API for the BOSC website. You can use this API to interact
+  with the BOSC website.
+  </p>
+  <a href="/api">API</a>
+    <h1> Login as a Club Executive</h1>
+    <form action="/admin" method="get">
+      <button type="submit">Login</button>
+    </form>
+  </body>
+</html>
+    `);
+});
 
 // Export the Hono app
 export default app;
